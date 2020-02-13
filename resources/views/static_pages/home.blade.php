571e3f774faa75a1c4c362b9a2ee0ffffd9e938c
yaojinhui <yaojinhui1993@gmail.com>
2020-02-14 07:28:49 +0800

    Initial style

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,19 @@
 
 @section('title', 'Home')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页
+        </p>
+
+        <p>
+            Now, starting from here.
+        </p>
+
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
+
 @endsection
