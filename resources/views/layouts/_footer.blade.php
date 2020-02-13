d9297d97ba5111ae5d34203788deec6ca32052cf
yaojinhui <yaojinhui1993@gmail.com>
2020-02-14 07:40:01 +0800

    Split header and footer views

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+    <a href="#">刻意练习，每日精进</a>
+
+    <div class="float-right">
+        <a href="#" class="">关于</a>
+    </div>
+</footer>
