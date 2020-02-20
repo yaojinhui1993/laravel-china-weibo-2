07f73c1eb4d03c4049a59a87fabe5a252fe812a1
yaojinhui <yaojinhui1993@gmail.com>
2020-02-20 20:08:10 +0800

    feat: user follow and unfollow other user
    1. add follow method in UserPolicy
    2. Finished!

---
+++ app/Http/Controllers/FollowersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FollowersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request, User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if (! Auth::user()->isFollowing($user->id)) {
+            Auth::user()->follow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if (Auth::user()->isFollowing($user->id)) {
+            Auth::user()->unFollow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+}
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -90,4 +90,9 @@ class UserPolicy
     {
         //
     }
+
+    public function follow(User $user, User $model)
+    {
+        return $user->id !== $model->id;
+    }
 }
