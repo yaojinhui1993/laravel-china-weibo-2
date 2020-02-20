9251aef4e2878a26d39c2ec7d2098e02eeecb19c
yaojinhui <yaojinhui1993@gmail.com>
2020-02-20 08:31:49 +0800

    feat: add status model
    1. create statuses table and model.
    2. add belongsTo and hasMany relationship.

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
 
         return "http://www.gravatar.com/avatar/{$hash}?s={$size}";
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
